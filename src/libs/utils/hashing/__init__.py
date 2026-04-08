39ede89685387556ab352d46c966340511a921a2
Antes <markadanov.antes@gmail.com>
2026-04-08 18:21:34 +0300

    chore: move hashing utils into folder and add verify function

---
+++ src/libs/utils/hashing/__init__.py
@@ -0,0 +1,3 @@
+from .hashing import hash_with_bcrypt, verify_bcrypt_hash
+
+__all__ = ["hash_with_bcrypt", "verify_bcrypt_hash"]
--- src/libs/utils/hashing/hashing.py
+++ src/libs/utils/hashing/hashing.py
@@ -8,3 +8,13 @@ def hash_with_bcrypt(plain_text: str) -> str:
         plain_text.encode("utf-8"),
         bcrypt.gensalt(),
     ).decode("utf-8")
+
+
+def verify_bcrypt_hash(
+    plain_text: str,
+    hashed_text: str,
+) -> bool:
+    return bcrypt.checkpw(
+        plain_text.encode("utf-8"),
+        hashed_text.encode("utf-8"),
+    )
