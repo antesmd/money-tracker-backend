6e746083bae2640aab197f46cf56909b40c56f60
Antes <markadanov.antes@gmail.com>
2026-04-20 22:35:06 +0300

    refactor: add naive boolean flag for datetime utils

--- src/libs/utils/datetime_utils.py
+++ src/libs/utils/datetime_utils.py
@@ -5,17 +5,30 @@ from datetime import UTC, datetime, timezone
 
 class DateTimeUtils:
     @staticmethod
-    def now(timezone: timezone = UTC) -> datetime:
-        return datetime.now(timezone)
+    def now(*, timezone: timezone = UTC, naive: bool = True) -> datetime:
+        dt = datetime.now(timezone)
+        if naive:
+            dt = dt.replace(tzinfo=None)
+
+        return dt
 
     @classmethod
-    def utc_now(cls) -> datetime:
-        return cls.now(UTC)
+    def utc_now(cls, *, naive: bool = True) -> datetime:
+        return cls.now(timezone=UTC, naive=naive)
 
     @staticmethod
-    def from_timestamp(timestamp: float, timezone: timezone = UTC) -> datetime:
-        return datetime.fromtimestamp(timestamp, timezone)
+    def from_timestamp(
+        timestamp: float,
+        *,
+        timezone: timezone = UTC,
+        naive: bool = True,
+    ) -> datetime:
+        dt = datetime.fromtimestamp(timestamp, timezone)
+        if naive:
+            dt = dt.replace(tzinfo=None)
+
+        return dt
 
     @classmethod
-    def utc_from_timestamp(cls, timestamp: float) -> datetime:
-        return cls.from_timestamp(timestamp, UTC)
+    def utc_from_timestamp(cls, timestamp: float, *, naive: bool = True) -> datetime:
+        return cls.from_timestamp(timestamp, timezone=UTC, naive=naive)
