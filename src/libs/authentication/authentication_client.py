958123bde8413cbdfd6e44c902a098fd6c5067dc
Antes <markadanov.antes@gmail.com>
2026-04-21 13:56:19 +0300

    chore: change environment variables names

--- src/libs/authentication/authentication_client.py
+++ src/libs/authentication/authentication_client.py
@@ -5,15 +5,15 @@ from typing import TYPE_CHECKING
 
 from fastapi import HTTPException, Request, status
 
-from src.libs.constants.environment.authentication import AUTHENTICATION_JWT_PUBLIC_KEY
+from src.libs.constants.environment.authentication import AUTHENTICATION_JWT_PUBLIC_KEY_PATH
 from src.libs.utils.jwt.exceptions import BaseJWTError
 from src.libs.utils.jwt.implementations.pyjwt import PyJWT
 
 if TYPE_CHECKING:
-    from src.identity.infrastructure.token_service import TokenPayload
     from src.libs.utils.jwt import IJWT
+    from src.modules.identity.infrastructure.token_service import TokenPayload
 
-public_key_path = Path(AUTHENTICATION_JWT_PUBLIC_KEY)
+public_key_path = Path(AUTHENTICATION_JWT_PUBLIC_KEY_PATH)
 public_key = public_key_path.read_text()
 
 
